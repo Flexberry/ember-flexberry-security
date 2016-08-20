9abd8179da5a8744635eb9e282eca9a8509de78b
Bratchikov Igor <ember@macbook.local>
2016-08-20 10:21:17 +0500

    Add limits for security forms

--- addon/routes/i-c-s-soft-s-t-o-r-m-n-e-t-security-group-l.js
+++ addon/routes/i-c-s-soft-s-t-o-r-m-n-e-t-security-group-l.js
@@ -1,4 +1,6 @@
+import Ember from 'ember';
 import ListFormRoute from 'ember-flexberry/routes/list-form';
+import { Query } from 'ember-flexberry-data';
 
 export default ListFormRoute.extend({
   /**
@@ -43,4 +45,32 @@ export default ListFormRoute.extend({
     @default {}
   */
   developerUserSettings: { ICSSoftSTORMNETSecurityGroupL: {} },
+  /**
+    It overrides base method and forms the limit predicate for loaded data.
+    If there is displayed even number or records per page, records where 'address' attribute contains letter 'S' are filtered.
+    If there is displayed odd number or records per page, records where 'address' attribute contains letter 'п' are filtered.
+
+    @public
+    @method objectListViewLimitPredicate
+    @param {Object} options Method options.
+    @param {String} [options.modelName] Type of records to load.
+    @param {String} [options.projectionName] Projection name to load data by.
+    @param {String} [options.params] Current route query parameters.
+    @return {BasePredicate} The predicate to limit loaded data.
+   */
+  objectListViewLimitPredicate: function(options) {
+    let methodOptions = Ember.merge({
+      modelName: undefined,
+      projectionName: undefined,
+      params: undefined
+    }, options);
+
+    if (methodOptions.modelName === this.get('modelName') &&
+        methodOptions.projectionName === this.get('modelProjection')) {
+      let limitFunction = new Query.SimplePredicate('isGroup', Query.FilterOperator.Eq, true);
+      return limitFunction;
+    }
+
+    return undefined;
+  }
 });
