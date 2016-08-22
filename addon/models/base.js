4edcb0867badcb051d73588c43a929e0942e1543
Bratchikov Igor <bratchikov@ics.perm.ru>
2016-08-22 17:03:52 +0500

    Add offline mixin for all models in addon

--- addon/models/base.js
+++ addon/models/base.js
@@ -1,4 +1,5 @@
 import BaseModel from 'ember-flexberry/models/base';
+import { Offline } from 'ember-flexberry-data';
 
-export default BaseModel.extend({
+export default BaseModel.extend(Offline.ModelMixin, {
 });
--- tests/dummy/app/models/ember-flexberry-dummy-application-user.js
+++ tests/dummy/app/models/ember-flexberry-dummy-application-user.js
@@ -1,8 +1,7 @@
 import __BaseModel from './base';
-import { Offline } from 'ember-flexberry-data';
 import { Model as ApplicationUserMixin, defineProjections } from '../mixins/regenerated/models/ember-flexberry-dummy-application-user';
 
-let Model = __BaseModel.extend(ApplicationUserMixin, Offline.ModelMixin, {
+let Model = __BaseModel.extend(ApplicationUserMixin, {
 
 });
 defineProjections(Model);
