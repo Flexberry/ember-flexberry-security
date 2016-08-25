60647be9187a7eef3516bca3141a06e441e6c9be
Bratchikov Igor <bratchikov@ics.perm.ru>
2016-08-25 16:24:45 +0500

    Fix audit entity preview form

--- addon/controllers/i-c-s-soft-s-t-o-r-m-n-e-t-business-audit-objects-audit-entity-e.js
+++ addon/controllers/i-c-s-soft-s-t-o-r-m-n-e-t-business-audit-objects-audit-entity-e.js
@@ -4,4 +4,5 @@ export default EditFormController.extend({
   // Caption of this particular edit form.
 
   parentRoute: 'i-c-s-soft-s-t-o-r-m-n-e-t-business-audit-objects-audit-entity-l',
+  readonly: true
 });
