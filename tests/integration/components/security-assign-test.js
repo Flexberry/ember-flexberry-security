b63e7730fb6c537715cef89afd6bb4d606f443ff
Bratchikov Igor <bratchikov@ics.perm.ru>
2016-09-06 17:24:59 +0500

    Fix security-assign-test

--- tests/integration/components/security-assign-test.js
+++ tests/integration/components/security-assign-test.js
@@ -1,17 +1,41 @@
+import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import SecurityAssignDataObject from 'ember-flexberry-security/objects/security-assign-data';
 
 moduleForComponent('security-assign', 'Integration | Component | security assign', {
-  integration: true
+  integration: true,
+
+  beforeEach: function () {
+    Ember.Component.reopen({
+      i18n: Ember.inject.service('i18n')
+    });
+  }
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{security-assign}}`);
+  this.set('securityAssign', SecurityAssignDataObject.create({
+      headers: ['Name', 'Own', 'Inherit'],
+      rows: [{
+        name: 'admin',
+        columns: [{ checked: true, readonly: false },
+                  { checked: true, readonly: true }
+        ]
+      },
+        {
+          name: 'Все пользователи',
+          columns: [{ checked: true, readonly: false },
+                    { checked: true, readonly: true }
+          ]
+        },
+      ],
+      hasContent: true
+    }));
 
-  assert.equal(this.$().text().trim(), '');
+  this.render(hbs`{{security-assign value=securityAssign}}`);
+
+  assert.notEqual(this.$().text().indexOf('Inherit'), -1);
 
   // Template block usage:
   this.render(hbs`
@@ -20,5 +44,5 @@ test('it renders', function(assert) {
     {{/security-assign}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().indexOf('template block text'), -1);
 });
