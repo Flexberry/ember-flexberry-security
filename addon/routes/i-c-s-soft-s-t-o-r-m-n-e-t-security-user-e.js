232922f60babe5ac8f99050ef76ec3c50838c812
Bratchikov Igor <bratchikov@ics.perm.ru>
2016-09-06 18:23:23 +0500

    Template security-user-e route for set userRoles

--- addon/routes/i-c-s-soft-s-t-o-r-m-n-e-t-security-user-e.js
+++ addon/routes/i-c-s-soft-s-t-o-r-m-n-e-t-security-user-e.js
@@ -1,6 +1,54 @@
 import EditFormRoute from 'ember-flexberry/routes/edit-form';
+import SecurityAssignDataObject from '../objects/security-assign-data';
 
 export default EditFormRoute.extend({
   modelProjection: 'UserE',
-  modelName: 'i-c-s-soft-s-t-o-r-m-n-e-t-security-agent'
+  modelName: 'i-c-s-soft-s-t-o-r-m-n-e-t-security-agent',
+  userRoles: null,
+
+  /**
+    A hook you can implement to convert the URL into the model for this route.
+    [More info](http://emberjs.com/api/classes/Ember.Route.html#method_model).
+
+    @method model
+    @param {Object} params
+    @param {Object} transition
+   */
+  model(params, transition) {
+    return this._super.apply(this, arguments).then(model => {
+      // TODO: load data. set userRoles
+
+      this.set('userRoles', SecurityAssignDataObject.create({
+        headers: ['Наименование', 'Своё', 'Унаследовано'],
+        rows: [{
+          name: 'admin',
+          columns: [{ checked: false, readonly: false },
+            { checked: true, readonly: true },
+          ]
+        },
+          {
+            name: 'Все пользователи',
+            columns: [{ checked: true, readonly: false },
+              { checked: true, readonly: true }
+            ]
+          },
+        ],
+        hasContent: true
+      }));
+      return model;
+    });
+  },
+
+  /**
+    A hook you can use to setup the controller for the current route.
+    [More info](http://emberjs.com/api/classes/Ember.Route.html#method_setupController).
+
+    @method setupController
+    @param {Ember.Controller} controller
+    @param {Object} model
+  */
+  setupController(controller) {
+    this._super(...arguments);
+    controller.set('userRoles', this.userRoles);
+  }
 });
