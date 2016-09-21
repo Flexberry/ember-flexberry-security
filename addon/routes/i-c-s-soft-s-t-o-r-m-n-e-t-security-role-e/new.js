11804557a52b461c24a98f5467a721b5b419d7b2
Bratchikov Igor <bratchikov@ics.perm.ru>
2016-09-21 18:37:42 +0500

    New role-e form with assigns

--- addon/routes/i-c-s-soft-s-t-o-r-m-n-e-t-security-role-e/new.js
+++ addon/routes/i-c-s-soft-s-t-o-r-m-n-e-t-security-role-e/new.js
@@ -1,13 +1,34 @@
+import Ember from 'ember';
 import EditFormNewRoute from 'ember-flexberry/routes/edit-form-new';
+import SecurityRoleERouteMixin from '../../mixins/security-role-e-route';
 
-export default EditFormNewRoute.extend({
+export default EditFormNewRoute.extend(SecurityRoleERouteMixin, {
   modelProjection: 'RoleOrGroupE',
   modelName: 'i-c-s-soft-s-t-o-r-m-n-e-t-security-agent',
   templateName: 'i-c-s-soft-s-t-o-r-m-n-e-t-security-role-e',
-  afterModel: function (model) {
-    model.set('isUser', false);
-    model.set('isGroup', false);
-    model.set('isRole', true);
-    model.set('enabled', true);
+
+  afterModel: function(model) {
+    return new Ember.RSVP.Promise((resolve, reject) => {
+
+      model.set('isUser', false);
+      model.set('isGroup', false);
+      model.set('isRole', true);
+      model.set('enabled', true);
+
+      this.fillData(model).then(()=> { resolve(); }).catch(error => { reject(error); });
+    });
+  },
+
+  /**
+    A hook you can use to setup the controller for the current route.
+    [More info](http://emberjs.com/api/classes/Ember.Route.html#method_setupController).
+
+    @method setupController
+    @param {Ember.Controller} controller
+    @param {Object} model
+  */
+  setupController(controller) {
+    this._super(...arguments);
+    this.setControllerVariables(controller);
   }
 });
