aa37b21623a4f9c793d8046f0a5bfa8d22cbee6b
Evgeniy Khaberev <ehaberev@yandex.ru>
2016-12-05 17:03:06 +0500

    Forgot to add some stuff to release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ This project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
 
+## [0.1.0-beta.1] - 2016-12-05
+### Added
+* Forms for editing client side audit data.
+
 ## [0.1.0-beta.0] - 2016-09-05
 ### Added
 * Forms for viewing client side audit data.
--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-flexberry-security'
+  name: 'ember-flexberry-security',
+
+  included: function(app) {
+    this._super.included.apply(this._super, arguments);
+
+    app.import('vendor/ember-flexberry-security/register-version.js');
+  }
 };
