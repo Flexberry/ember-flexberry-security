1993c9d6f8dd9d06da0b0aadd3223df4c0dcbb8d
ics_home\ananin <anton-iv87@yandex.ru>
2021-07-09 11:25:16 +0500

    Fix transform.

--- addon/transforms/audit-big-data-serialized-fields.js
+++ addon/transforms/audit-big-data-serialized-fields.js
@@ -1,24 +1,13 @@
-/**
-  @module ember-flexberry-data
-*/
-
 import Ember from 'ember';
-import StringTransform from 'ember-data/-private/transforms/string';
+import DS from 'ember-data';
 
 /**
   Transformation for model's attributes defined as <a href="http://emberjs.com/api/data/#method_attr">DS.attr</a> with type 'string'.
-  It extends <a href="http://emberjs.com/api/data/classes/DS.StringTransform.html">number transformation</a> from ember data.
+  It extends <a href="http://emberjs.com/api/data/classes/DS.StringTransform.html">string transformation</a> from ember data.
   @class AuditBigDataSerializedFieldsTransform
   @extends <a href="http://emberjs.com/api/data/classes/DS.StringTransform.html">DS.StringTransform</a>
-  @example
-  ```
-  import DS from 'ember-data';
-  export default DS.Model.extend({
-    externalId: DS.attr('guid')
-  });
-  ```
 */
-export default StringTransform.extend({
+export default DS.StringTransform.extend({
   /**
     Deserializes serialized attribute value.
    */
