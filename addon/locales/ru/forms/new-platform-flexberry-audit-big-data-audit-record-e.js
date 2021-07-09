af2fd45233d99eef6bc1ff0507d9bd6ed60650b0
ics_home\ananin <anton-iv87@yandex.ru>
2021-07-09 16:33:10 +0500

    Fix.

--- addon/locales/ru/forms/new-platform-flexberry-audit-big-data-audit-record-e.js
+++ addon/locales/ru/forms/new-platform-flexberry-audit-big-data-audit-record-e.js
@@ -1,15 +1,15 @@
 export default {
   caption: 'Запись аудита',
   'objectPrimaryKey-caption': 'Идентификатор',
-  'objectType-caption': 'Тип Объекта',
-  'operationTime-caption': 'Время Операции',
-  'operationType-caption': 'Тип Операции',
+  'objectType-caption': 'Тип объекта',
+  'operationTime-caption': 'Время операции',
+  'operationType-caption': 'Тип операции',
   'executionStatus-caption': 'Статус',
   'source-caption': 'Источник',
-  'userName-caption': 'Имя Пользователя',
+  'userName-caption': 'Имя пользователя',
   'userLogin-caption': 'Логин',
   'serializedFields-caption': 'Изменения',
-  'serializedFields-name-column-caption': 'Имя Поля',
-  'serializedFields-old-value-column-caption': 'Старое Значение',
-  'serializedFields-new-value-column-caption': 'Новое Значение'
+  'serializedFields-name-column-caption': 'Имя поля',
+  'serializedFields-old-value-column-caption': 'Старое значение',
+  'serializedFields-new-value-column-caption': 'Новое значение'
 };
