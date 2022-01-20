3070a03ebb2c344305bd95623a1675c3da714da8
ronzhak <manamain@gmail.com>
2022-01-20 15:41:05 +0500

    Fix sourceType for TypeAccessEnum (#33)
    Co-authored-by: dronzhin <dronzhin@pminst.ru>

--- addon/transforms/i-c-s-soft-s-t-o-r-m-n-e-t-security-t-type-access.js
+++ addon/transforms/i-c-s-soft-s-t-o-r-m-n-e-t-security-t-type-access.js
@@ -2,5 +2,6 @@ import FlexberryEnum from 'ember-flexberry-data/transforms/flexberry-enum';
 import tTypeAccessEnum from '../enums/i-c-s-soft-s-t-o-r-m-n-e-t-security-t-type-access';
 
 export default FlexberryEnum.extend({
-  enum: tTypeAccessEnum
+  enum: tTypeAccessEnum,
+  sourceType: 'ICSSoft.STORMNET.Security.tTypeAccess'
 });
