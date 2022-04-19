574e8eb7ab34357f54cff5c952e30375132a8416
Churekov Valentin <VChurekov@ics.perm.ru>
2022-04-19 14:52:56 +0500

    PR fix 3.

--- addon/mixins/regenerated/serializers/addon/models/new-platform-flexberry-audit-big-data-audit-record.js
+++
@@ -1,7 +0,0 @@
-import { Model as AuditRecordMixin, defineProjections } from '../mixins/regenerated/models/new-platform-flexberry-audit-big-data-audit-record';
-import __BaseModel from './base';
-let Model = __BaseModel.extend(AuditRecordMixin, {
-
-});
-defineProjections(Model);
-export default Model;
